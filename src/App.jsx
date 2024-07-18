8180e18efb1102ba81be29587eac077599fa3a3e
Hamed abdollahzade <hamedabdollahzade.ab@gmail.com>
2024-07-18 15:25:51 +0330

    up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Extensions from "./Page/Extensions";
+
+function App() {
+  return (
+    <div className="bg-red-400 w-full flex flex-col">
+      <Extensions />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Page/Extensions.jsx
@@ -0,0 +1,38 @@
+const Extensions = () => {
+  const ex = [
+    "Live Server",
+    "Better Comments",
+    "Auto Rename Tag",
+    "Auto Rename Tag",
+    "Blackbox",
+    "Code Spell Checker",
+    "Color Highlight",
+    "Easy icon theme",
+    "ES7+ React/Redux/React-Native snippets",
+    "ESLint",
+    "GitLens — Git supercharged",
+    "Import Cost",
+    "Prettier - Code formatter",
+    "Quokka.js",
+    "Tabnine",
+    "Tailwind CSS IntelliSense",
+  ];
+
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-4xl p-2 font-bold">vs code extensions for front end developer :</h1>
+      <div className="p-4">
+        <ul>
+          {ex.map((item, index) => (
+            <li key={index}>
+              {" "}
+              {index + 1} - {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Extensions;
